5551eddcefdede778cb58829b97b1c0358410caf
Snowballxld <65568323+Snowballxld@users.noreply.github.com>
2025-04-02 16:26:29 -0700

    What if we just use an api call?

--- Frontend/scripts/filehandler.js
+++ Frontend/scripts/filehandler.js
@@ -1,6 +1,6 @@
 // Recording variables
-const site = "https://comp4537termproject-2.onrender.com";
-// const site = ""
+//const site = "https://comp4537termproject-2.onrender.com";
+const site = ""
 let mediaRecorder;
 let audioStream;
 let audioChunks = [];
